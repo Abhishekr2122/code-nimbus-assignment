e5d0fab659cc66fdd4713d2b0bd33a903ad8311f
Abhishekr2122 <abhishekrshinde21@gmail.com>
2024-05-11 17:34:53 +0530

    Built nav element.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,64 @@
+import HeartIcon from "../ui/HeartIcon";
 import "./navbar.css";
-
+import { TbLetterG } from "react-icons/tb";
+import { IoIosHeart } from "react-icons/io";
+import { TbLetterD } from "react-icons/tb";
 export default function NavBar() {
   return (
     <div className="nav">
-      <div className="content-container"></div>
+      <div className="content-container">
+        <div className="content">
+          <div className="text-container">HOME</div>
+          <div>
+            <HeartIcon />
+          </div>
+        </div>
+        <div className="content">
+          <div className="text-container">ABOUT</div>
+          <div>
+            <HeartIcon />
+          </div>
+        </div>
+        <div className="content">
+          <div className="text-container">GALLERY</div>
+          <div>
+            <HeartIcon />
+          </div>
+        </div>
+        <div className="content">
+          <div className="text-container">THE STORY</div>
+        </div>
+
+        <div className="content">
+          <div className="logo-container">
+            <TbLetterG />
+            <IoIosHeart />
+            <TbLetterD />
+          </div>
+        </div>
+
+        <div className="content">
+          <div className="text-container">THE WEDDING</div>
+          <div>
+            <HeartIcon />
+          </div>
+        </div>
+        <div className="content">
+          <div className="text-container">BLOG</div>
+          <div>
+            <HeartIcon />
+          </div>
+        </div>
+        <div className="content">
+          <div className="text-container">PAGE</div>
+          <div>
+            <HeartIcon />
+          </div>
+        </div>
+        <div className="content">
+          <div className="text-container">RSVP</div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/ui/HeartIcon.js
@@ -0,0 +1,5 @@
+import { IoHeartSharp } from "react-icons/io5";
+
+export default function HeartIcon() {
+  return <IoHeartSharp style={{ color: "red", height: "80%", width: "80%" }} />;
+}
